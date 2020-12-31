be664145445b2c4806255198f263cbd7fabe9175
Florian Coulmier <florian.coulmier@vadesecure.com>
2020-12-31 18:09:40 +0100

    Change thresholds for fan control

--- recalbox_ControlFan.py
+++ recalbox_ControlFan.py
@@ -6,12 +6,15 @@ from multiprocessing import Process
 #initialize pins
 fanPin = 21 #pin 40
 
+# Temperatures at which the fan start and stop
+# Use different temperatures to avoid frequent start/stop
+startThreshold = 55
+stopThreshold = 45
+
 fanFlag = GPIO.HIGH
 
 #initialize GPIO settings
 def init():
-    global fanFlag
-    global fanPin
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(fanPin, GPIO.OUT)
     startFan()
@@ -19,29 +22,27 @@ def init():
 
 def startFan():
     global fanFlag
-    global fanPin
     fanFlag = GPIO.HIGH
     print "Starting Fan"
     GPIO.output(fanPin, fanFlag)
 
 def stopFan():
     global fanFlag
-    global fanPin
     fanFlag = GPIO.LOW
     print "Stopping Fan"
     GPIO.output(fanPin, fanFlag)
 
 #waits for user to hold button up to 1 second before issuing poweroff command
 def checkTemperature():
-    global fanFlag
     while True:
         with open("/sys/class/thermal/thermal_zone0/temp") as f:
             temp = float(f.read().strip()) / 1000
 
-        print "Temperature = %.2f / Fan Status = %s" % (temp, fanFlag)
-        if temp >= 50 and fanFlag != GPIO.HIGH:
+        print "Temperature = %.2f / Fan Status = %s / start: %d / stop: %d" % \
+                (temp, fanFlag, startThreshold, stopThreshold)
+        if temp >= startThreshold and fanFlag != GPIO.HIGH:
             startFan()
-        elif temp <= 45 and fanFlag != GPIO.LOW:
+        elif temp < stopThreshold and fanFlag != GPIO.LOW:
             stopFan()
 
         time.sleep(5)
